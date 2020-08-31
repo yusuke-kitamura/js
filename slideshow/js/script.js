59d8a0df4378562a934b3501d02020ec182c9b1e
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-08-31 18:08:41 +0900

    [add]code to skip images

---
+++ slideshow/js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skipper({
+	translation : 'fade',
+	speed : 800,
+	easing : 'easeOutQuart',
+	navType: 'block',
+	childrenElementType: 'div'
+	arrows : true,
+	autoplay : false,
+	autoPlayDuration : 5000,
+	keyboardOnAlways: true,
+	hidePrevious : false
+});
